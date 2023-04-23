ac16bcd93f26bed5c64875ffb732ec92a7dc52bf
Erik395 <131557036+Erik395@users.noreply.github.com>
2023-04-23 10:32:25 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
+        OMG_Tank.Set_Speed(100, 100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_UP) {
+        OMG_Tank.Set_Speed(0, 0)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
+        OMG_Tank.Set_Speed(-100, -100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_UP) {
+        OMG_Tank.Set_Speed(0, 0)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
+        OMG_Tank.Set_Speed(-100, 100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_UP) {
+        OMG_Tank.Set_Speed(0, 0)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
+        OMG_Tank.Set_Speed(100, -100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_UP) {
+        OMG_Tank.Set_Speed(0, 0)
+    } else {
+        OMG_Tank.Set_Speed(0, 0)
+    }
+})
+OMG_Tank.Tank_Init(Tank_version.V3)
+OMG_Tank.Stop_Motors()
+OMG_Tank.Set_Speed(0, 0)
